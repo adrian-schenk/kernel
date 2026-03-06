442c4179736835e6565e6f3a46f320f0f4b34b85
FearXenon <118049326+FearXenon@users.noreply.github.com>
2026-03-06 17:02:35 +0100

    refactor memory layout documentation and clean up unused variables in pm.c

--- arch/x86/boot/pm.c
+++ arch/x86/boot/pm.c
@@ -1,14 +1,10 @@
 #include <memlayout.h>
 #include <boot_info.h>
 
-extern uint64_t page_table_l4;
+extern uint64_t pt_ptr[];
 
 void set_ap_boot_info() {
     struct ap_boot_info* boot_info = (struct ap_boot_info*) AP_BOOT_INFO_ADDR;
 
-    //boot_info->l4_table_phys = page_table_l4;
-
-    
-
-    
+    return;
 }
