8803daad26cbabbd4b587a1c19bc047bebe7fbfa
Adrian Schenk <118049326+adrian-schenk@users.noreply.github.com>
2026-06-03 09:34:10 +0200

    added `union` for foreground and background colors

--- kernel/graphics.c
+++ kernel/graphics.c
@@ -1,34 +1,41 @@
 #include "graphics.h"
 
-static int plot_pixel(int x, int y, char* data, graphics_color_t t){
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            uint8_t* v = (video_buffer + ((x + j) + (y + i) * video_xres) * 3);
-            if((data[i] >> (7-j) & 1)){
-                v[0] = t.blue;
-                v[1] = t.green;
-                v[2] = t.red;
-            }
-            else{
-                v[0] = t.bg;
-                v[1] = t.bg;
-                v[2] = t.bg;
-            }
-        }
+static int plot_pixel(int x, int y, char *data, graphics_color_t t)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      uint8_t *v = (video_buffer + ((x + j) + (y + i) * video_xres) * 3);
+      if ((data[i] >> (7 - j) & 1))
+      {
+        v[0] = t.blue;
+        v[1] = t.green;
+        v[2] = t.red;
+      }
+      else
+      {
+        v[0] = t.b_bg;
+        v[1] = t.g_bg;
+        v[2] = t.r_bg;
+      }
     }
+  }
 }
 
-void graphics_draw_char(int x, int y, char c, graphics_color_t t){
-    plot_pixel(x, y, &fontdata[c*8], t);
+void graphics_draw_char(int x, int y, char c, graphics_color_t t)
+{
+  plot_pixel(x, y, &fontdata[c * 8], t);
 }
 
-int graphics_draw_pixel(int x, int y, int color){
-    // 3 bytes per pixel => 24 bit colors
-    uint8_t* v = (video_buffer + ((x) + (y) * video_xres) * 3);
+int graphics_draw_pixel(int x, int y, int color)
+{
+  // 3 bytes per pixel => 24 bit colors
+  uint8_t *v = (video_buffer + ((x) + (y)*video_xres) * 3);
 
-    v[2] = color >> 24;
-    v[1] = color >> 16;
-    v[0] = color >> 8;
+  v[2] = color >> 24;
+  v[1] = color >> 16;
+  v[0] = color >> 8;
 
-    return 1;
+  return 1;
 }
--- kernel/graphics.h
+++ kernel/graphics.h
@@ -5,16 +5,31 @@
 
 #include "globals.h"
 
-typedef struct graphics {
-    uint8_t* video_buffer;
+typedef struct graphics
+{
+  uint8_t *video_buffer;
 } graphics_t;
 
-typedef struct graphics_color{
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
-    uint8_t alpha;
-    uint8_t bg;
+typedef struct graphics_color
+{
+  union {
+    struct {
+      uint8_t red;
+      uint8_t green;
+      uint8_t blue;
+      uint8_t alpha;
+    };
+    uint32_t color;
+  };
+  union  {
+    struct {
+      uint8_t r_bg;
+      uint8_t g_bg;
+      uint8_t b_bg;
+      uint8_t a_bg;
+    };
+    uint32_t bg_color;
+  };
 } graphics_color_t;
 
 void graphics_draw_char(int x, int y, char c, graphics_color_t);
