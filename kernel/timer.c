e41f1560c12c34a9983ba665bcda7b2a280e296b
Adrian Schenk <118049326+adrian-schenk@users.noreply.github.com>
2026-06-03 11:18:48 +0200

    format code

--- kernel/timer.c
+++ kernel/timer.c
@@ -14,114 +14,129 @@ const int rtc_calibration_ticks = 256;
 long long apic_speed = 0;
 
 // will only run on bs core
-void rtc_interrupt(uint64_t interrupt_number, uint64_t error_code) {
-    rtc_ticks++;
-    outb(0x70, 0x0C);	// select register C
-    inb(0x71);		// just throw away contents
+void rtc_interrupt(uint64_t interrupt_number, uint64_t error_code)
+{
+  rtc_ticks++;
+  outb(0x70, 0x0C); // select register C
+  inb(0x71);        // just throw away contents
 }
 
 // will run on all cores
-void apic_timer_setup_interrupt(uint64_t interrupt_number, uint64_t error_code) {
-    this_cpu(apic_ticks)++;
+void apic_timer_setup_interrupt(uint64_t interrupt_number, uint64_t error_code)
+{
+  this_cpu(apic_ticks)++;
 }
 
 // will run on all cores
-void apic_interrupt(uint64_t interrupt_number, uint64_t error_code) {
-    this_cpu(apic_time) += this_cpu(ms_counter);
-    this_cpu(last_schedule) += this_cpu(ms_counter);
-    if (this_cpu(last_schedule) > this_cpu(ms_counter) * 2) {
-        this_cpu(last_schedule) = 0;
-        scheduler_tick(this_cpu(scheduler));
-    }
+void apic_interrupt(uint64_t interrupt_number, uint64_t error_code)
+{
+  this_cpu(apic_time) += this_cpu(ms_counter);
+  this_cpu(last_schedule) += this_cpu(ms_counter);
+  if (this_cpu(last_schedule) > this_cpu(ms_counter) * 2)
+  {
+    this_cpu(last_schedule) = 0;
+    scheduler_tick(this_cpu(scheduler));
+  }
 }
 
-void timer_setup() {
+void timer_setup()
+{
 
-    // enable RTC interrupts for apic timer calibration
-    cli();
-    set_interrupt_handler(this_cpu(interrupt_handlers), 40, rtc_interrupt);
-    uint32_t low =
-        0x28          // vector
-        | (0 << 8)    // fixed delivery
-        | (0 << 11)   // physical destination
-        | (0 << 13)   // active high
-        | (0 << 15)   // edge trigger
-        | (0 << 16);  // unmasked
+  // enable RTC interrupts for apic timer calibration
+  cli();
+  set_interrupt_handler(this_cpu(interrupt_handlers), 40, rtc_interrupt);
+  uint32_t low =
+      0x28         // vector
+      | (0 << 8)   // fixed delivery
+      | (0 << 11)  // physical destination
+      | (0 << 13)  // active high
+      | (0 << 15)  // edge trigger
+      | (0 << 16); // unmasked
 
-    ioapic_write(0x20, low);
-    uint32_t high = (0 << 24);
-    ioapic_write(0x21, high);
+  ioapic_write(0x20, low);
+  uint32_t high = (0 << 24);
+  ioapic_write(0x21, high);
 
-    outb(0x70, 0x8B); // select register B and disable NMI for RTC interrupts
-    char prev=inb(0x71);	// read the current value of register B
-    outb(0x70, 0x8B);		// set the index again (a read will reset the index to register D)
-    outb(0x71, prev | 0x40);	// write the previous value ORed with 0x40. This turns on bit 6 of register B
-    sti();
+  outb(0x70, 0x8B);        // select register B and disable NMI for RTC interrupts
+  char prev = inb(0x71);   // read the current value of register B
+  outb(0x70, 0x8B);        // set the index again (a read will reset the index to register D)
+  outb(0x71, prev | 0x40); // write the previous value ORed with 0x40. This turns on bit 6 of register B
+  sti();
 
-    // apic setup
-    set_interrupt_handler(this_cpu(interrupt_handlers), 32, apic_timer_setup_interrupt);
-    apic_write(TIMER_DIVIDE_CONFIGURATION_REGISTER, TIMER_DIVIDE_1); // divide by 1
-    
-    while (rtc_ticks < 2);
-    
-    apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_PERIODIC); // enable periodic timer
-    apic_write(TIMER_INITIAL_COUNT_REGISTER, 10240 << 2); // set counter
+  // apic setup
+  set_interrupt_handler(this_cpu(interrupt_handlers), 32, apic_timer_setup_interrupt);
+  apic_write(TIMER_DIVIDE_CONFIGURATION_REGISTER, TIMER_DIVIDE_1); // divide by 1
 
-    while (rtc_ticks < rtc_calibration_ticks + 2);
+  while (rtc_ticks < 2)
+    ;
 
-    apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_DISABLED); // disable apic timer
-    outb(0x70, 0x0B); // disable rtc interrupts
+  apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_PERIODIC); // enable periodic timer
+  apic_write(TIMER_INITIAL_COUNT_REGISTER, 10240 << 2);             // set counter
 
-    float seconds = rtc_ticks / 1024.0f;
+  while (rtc_ticks < rtc_calibration_ticks + 2)
+    ;
 
-    if (seconds > 0) {
-        apic_speed = (uint64_t)(((10240 << 2) * this_cpu(apic_ticks) / 2) / seconds);
-        printf("Detected APIC Timer speed: %d\n", apic_speed);
-    } else {
-        printf("APIC Timer could not be initialized\n");
-    }
+  apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_DISABLED); // disable apic timer
+  outb(0x70, 0x0B);                                                 // disable rtc interrupts
 
-    this_cpu(ms_counter) = apic_speed / 1000 * 2;
-    
-    set_interrupt_handler(this_cpu(interrupt_handlers), 32, apic_interrupt);
-    apic_write(TIMER_INITIAL_COUNT_REGISTER, this_cpu(ms_counter)); // set counter
-    apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_PERIODIC); // enable apic timer
+  float seconds = rtc_ticks / 1024.0f;
+
+  if (seconds > 0)
+  {
+    apic_speed = (uint64_t)(((10240 << 2) * this_cpu(apic_ticks) / 2) / seconds);
+    printf("Detected APIC Timer speed: %d\n", apic_speed);
+  }
+  else
+  {
+    printf("APIC Timer could not be initialized\n");
+  }
+
+  this_cpu(ms_counter) = apic_speed / 1000 * 2;
+
+  set_interrupt_handler(this_cpu(interrupt_handlers), 32, apic_interrupt);
+  apic_write(TIMER_INITIAL_COUNT_REGISTER, this_cpu(ms_counter));   // set counter
+  apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_PERIODIC); // enable apic timer
 }
 
-void timer_setup_ap(struct cpu_local *cpu_local) {
-    
-    apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_DISABLED); // disable apic timer (if enabled)
-    apic_write(TIMER_DIVIDE_CONFIGURATION_REGISTER, TIMER_DIVIDE_1); // divide by 1
-    apic_write(TIMER_INITIAL_COUNT_REGISTER, 10240 << 2); // set counter
+void timer_setup_ap(struct cpu_local *cpu_local)
+{
 
-    set_interrupt_handler(this_cpu(interrupt_handlers), 32, apic_timer_setup_interrupt);
+  apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_DISABLED); // disable apic timer (if enabled)
+  apic_write(TIMER_DIVIDE_CONFIGURATION_REGISTER, TIMER_DIVIDE_1);  // divide by 1
+  apic_write(TIMER_INITIAL_COUNT_REGISTER, 10240 << 2);             // set counter
 
-    boot_info->ap_startup_done = 1; // boot up other cores
+  set_interrupt_handler(this_cpu(interrupt_handlers), 32, apic_timer_setup_interrupt);
 
-    while (timer_phase < 1);
-    
-    apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_PERIODIC); // enable periodic timer
-    apic_write(TIMER_INITIAL_COUNT_REGISTER, 10240 << 2); // set counter
-    
-    while (timer_phase < 2);
+  boot_info->ap_startup_done = 1; // boot up other cores
 
-    apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_DISABLED); // disable apic timer
-    
-    // set regular interrupt handlers
-    cpu_local->interrupt_handlers = &interrupt_handlers;
+  while (timer_phase < 1)
+    ;
 
-    long long apic_speed = ((10240 << 2) * this_cpu(apic_ticks));
-    cpu_local->ms_counter = apic_speed / timer_calib_ms;
-    cpu_local->apic_time = 0;
+  apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_PERIODIC); // enable periodic timer
+  apic_write(TIMER_INITIAL_COUNT_REGISTER, 10240 << 2);             // set counter
 
-    apic_write(TIMER_INITIAL_COUNT_REGISTER, cpu_local->ms_counter); // set counter (for 1 ms)
-    apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_PERIODIC); // enable periodic timer (to enable sleep)
+  while (timer_phase < 2)
+    ;
+
+  apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_DISABLED); // disable apic timer
+
+  // set regular interrupt handlers
+  cpu_local->interrupt_handlers = &interrupt_handlers;
+
+  long long apic_speed = ((10240 << 2) * this_cpu(apic_ticks));
+  cpu_local->ms_counter = apic_speed / timer_calib_ms;
+  cpu_local->apic_time = 0;
+
+  apic_write(TIMER_INITIAL_COUNT_REGISTER, cpu_local->ms_counter);  // set counter (for 1 ms)
+  apic_write(LVT_TIMER_REGISTER, TIMER_INTERRUPT | TIMER_PERIODIC); // enable periodic timer (to enable sleep)
 }
 
-unsigned long long get_time() {
-    return this_cpu(apic_time);
+unsigned long long get_time()
+{
+  return this_cpu(apic_time);
 }
 
-int get_ms_counter() {
-    return this_cpu(ms_counter);
+int get_ms_counter()
+{
+  return this_cpu(ms_counter);
 }
